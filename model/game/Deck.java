002ced3da434474df5f9b762d25f7edaf571989f
Lance Ricco Teng <47263399+S0rbetes@users.noreply.github.com>
2019-08-10 16:29:48 +0800

    Fixed Go To Start Chance Card, converted initalization to a for-loop

--- model/game/Deck.java
+++ model/game/Deck.java
@@ -29,47 +29,41 @@ public class Deck {
     {
         Random rand = new Random();
 
-        Chance  GOJF1 = new GetOutOfJailChance(),
-                GOJF2 = new GetOutOfJailChance(),
+        for (int i = 0; i < 2; i++)
+        {
+            cards.add(new GetOutOfJailChance());
+        }
 
-                goToAsset1 = new MoveToSpaceChance(Chance.GO_TO_PROP + rand.nextInt(3), game.getBoard()),
-                goToAsset2 = new MoveToSpaceChance(Chance.GO_TO_PROP + rand.nextInt(3), game.getBoard()),
-                goToAsset3 = new MoveToSpaceChance(Chance.GO_TO_PROP + rand.nextInt(3), game.getBoard()),
-                goToAsset4 = new MoveToSpaceChance(Chance.GO_TO_PROP + rand.nextInt(3), game.getBoard()),
-                goToAsset5 = new MoveToSpaceChance(Chance.GO_TO_PROP + rand.nextInt(3), game.getBoard()),
-                goToAsset6 = new MoveToSpaceChance(Chance.GO_TO_PROP + rand.nextInt(3), game.getBoard()),
+        for (int i = 0; i < 6; i++)
+        {
+            cards.add(new MoveToSpaceChance(Chance.GO_TO_PROP + rand.nextInt(3), game.getBoard()));
+        }
 
-                getMoney1 = new ChangeMoneyChance(Chance.BANK_DIVIDEND + rand.nextInt(5), game.getBank()),
-                getMoney2 = new ChangeMoneyChance(Chance.BANK_DIVIDEND + rand.nextInt(5), game.getBank()),
-                getMoney3 = new ChangeMoneyChance(Chance.BANK_DIVIDEND + rand.nextInt(5), game.getBank()),
-                getMoney4 = new ChangeMoneyChance(Chance.BANK_DIVIDEND + rand.nextInt(5), game.getBank()),
-                getMoney5 = new ChangeMoneyChance(Chance.BANK_DIVIDEND + rand.nextInt(5), game.getBank()),
-                getMoney6 = new ChangeMoneyChance(Chance.BANK_DIVIDEND + rand.nextInt(5), game.getBank()),
+        for (int i = 0; i < 6; i++)
+        {
+            int temp = Chance.BANK_DIVIDEND + rand.nextInt(5);
+            if (temp == Chance.GO_TO_START)
+            {
+                cards.add(new MoveToSpaceChance(temp, game.getBoard()));
+            } else {
+                cards.add(new ChangeMoneyChance(temp, game.getBank()));
+            }
+        }
 
-                moveToSpace1 = new MoveToSpaceChance(Chance.GO_TO_JAIL + rand.nextInt(2), game.getBoard()),
-                moveToSpace2 = new MoveToSpaceChance(Chance.GO_TO_JAIL + rand.nextInt(2), game.getBoard()),
-                moveToSpace3 = new MoveToSpaceChance(Chance.GO_TO_JAIL + rand.nextInt(2), game.getBoard()),
-                moveToSpace4 = new MoveToSpaceChance(Chance.GO_TO_JAIL + rand.nextInt(2), game.getBoard()),
+        for (int i = 0; i < 4; i++)
+        {
+            cards.add(new MoveToSpaceChance(Chance.GO_TO_JAIL + rand.nextInt(2), game.getBoard()));
+        }
 
-                rentModifier1 = new RentModifierChance(Chance.DOUBLE_RENT + rand.nextInt(5)),
-                rentModifier2 = new RentModifierChance(Chance.DOUBLE_RENT + rand.nextInt(5)),
-                rentModifier3 = new RentModifierChance(Chance.DOUBLE_RENT + rand.nextInt(5)),
-                rentModifier4 = new RentModifierChance(Chance.DOUBLE_RENT + rand.nextInt(5)),
-                rentModifier5 = new RentModifierChance(Chance.DOUBLE_RENT + rand.nextInt(5)),
-                rentModifier6 = new RentModifierChance(Chance.DOUBLE_RENT + rand.nextInt(5)),
-                rentModifier7 = new RentModifierChance(Chance.DOUBLE_RENT + rand.nextInt(5)),
+        for (int i = 0; i < 7; i++)
+        {
+            cards.add(new RentModifierChance(Chance.DOUBLE_RENT + rand.nextInt(5)));
+        }
 
-                loseMoney1 = new ChangeMoneyChance(Chance.DONATE_MONEY + rand.nextInt(2), game.getBank()),
-                loseMoney2 = new ChangeMoneyChance(Chance.DONATE_MONEY + rand.nextInt(2), game.getBank()),
-                loseMoney3 = new ChangeMoneyChance(Chance.DONATE_MONEY + rand.nextInt(2), game.getBank());
-
-
-        cards.addAll(Arrays.asList (GOJF1, GOJF2,
-                goToAsset1, goToAsset2, goToAsset3, goToAsset4, goToAsset5, goToAsset6,
-                getMoney1, getMoney2, getMoney3, getMoney4, getMoney5, getMoney6,
-                moveToSpace1, moveToSpace2, moveToSpace3, moveToSpace4,
-                rentModifier1, rentModifier2, rentModifier3, rentModifier4, rentModifier5, rentModifier6, rentModifier7,
-                loseMoney1, loseMoney2, loseMoney3));
+        for (int i = 0; i < 3; i++)
+        {
+            cards.add(new ChangeMoneyChance(Chance.DONATE_MONEY + rand.nextInt(2), game.getBank()));
+        }
 
         shuffle();
     }
